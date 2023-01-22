5b1c10dbaa7538a98dc86f91da9741a05daa6d94
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-01-22 21:08:02 +0300

    handleInputChange

--- src/shared/ui/Input/Input.js
+++ src/shared/ui/Input/Input.js
@@ -1,22 +1,45 @@
-import classes from "./Input.module.scss";
+import classes from './Input.module.scss';
+import {
+  validateName,
+  validateTel,
+  validateConnect,
+  validateEmail,
+} from 'shared/lib';
+import { useState } from 'react';
 
 export const Input = ({
   type,
   placeholder,
   name,
   onInputChange,
-  setInput,
-  setIsValidInput,
   validateInput,
 }) => {
+  const [input, setInput] = useState(null);
+  const [isValidInput, setIsValidInput] = useState(false);
+
   const handleInputChange = (event) => {
     console.log(event.target.value);
     const value = event.target.value;
-    setInput(value);
-    setIsValidInput(validateInput(value));
+    setInput(event.target.value);
+
+    switch (type) {
+      case 'text':
+        setIsValidInput(validateName(value));
+        break;
+      case 'tel':
+        setIsValidInput(validateTel(value));
+        break;
+      case 'email':
+        setIsValidInput(validateEmail(value));
+        break;
+      default:
+        setIsValidInput(validateConnect(value));
+    }
+
+    console.log(isValidInput);
   };
 
-  if (type === "text" || type === "tel" || type === "email")
+  if (type === 'text' || type === 'tel' || type === 'email')
     return (
       <input
         className={classes.input}
@@ -27,5 +50,5 @@ export const Input = ({
       />
     );
 
-  if (type === "checkbox") return <input type={type} />;
+  if (type === 'checkbox') return <input type={type} />;
 };
