8a9e11847db7145665bece118f51fcbbdf5d1d9b
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2023-02-13 11:01:29 +0300

    WIP Header

--- src/widgets/Header/Header.js
+++ src/widgets/Header/Header.js
@@ -1,22 +1,65 @@
 import { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import classes from './Header.module.scss';
+import { classNames } from 'shared/lib/classNames';
 
 const HeaderPage = () => {
-    const lang='en'
-    const [data, setData] = useState(null)
+  const lang = 'en';
+  const theme = 'light';
+  const [isMenuActive, setIsMenuActive] = useState(false);
 
-    useEffect(() => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
     fetch(
       `https://zenproject-ce905-default-rtdb.firebaseio.com/${lang}/header.json`
     )
       .then((response) => response.json())
       .then((data) => {
         setData(data);
-        console.log(data)
+        console.log(data);
       })
       .catch();
   }, [lang]);
 
+  const handleItemClick = () => {
+    isMenuActive ? setIsMenuActive(false) : setIsMenuActive(true);
+  };
 
-}
+  const classNameMenu = classNames(
+    classes.menu,
+    {
+      [classes.active]: isMenuActive,
+      dark: theme === 'dark',
+    },
+    []
+  );
 
-export default HeaderPage
+  return (
+    <div>
+      {data && (
+        <ul className={classNameMenu} theme={theme}>
+          {data.menuItems.length > 0 &&
+            data.menuItems.map((menuItem, index) => (
+              <li
+                onClick={handleItemClick}
+                className={classes.item}
+                key={index}
+              >
+                <NavLink
+                  to={`/${menuItem.target}`}
+                  className={({ isActive }) =>
+                    isActive ? 'active' : 'inactive'
+                  }
+                >
+                  {menuItem.text}
+                </NavLink>
+              </li>
+            ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default HeaderPage;
