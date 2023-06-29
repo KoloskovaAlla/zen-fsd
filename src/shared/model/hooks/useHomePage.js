c97d11b30eeb6c4b905bf0700f5483b55c3756ea
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-06-29 18:54:20 +0300

    JSDoc in the useHomePage

--- src/shared/model/hooks/useHomePage.js
+++ src/shared/model/hooks/useHomePage.js
@@ -4,11 +4,10 @@ import { fetchHomePageData } from '../reducers/homeSlice';
 /** @type {(store: object) => object} */
 const callback = (store) => store.homePageReducer;
 /**
- * 
- *  
+ * @typedef {import('./types').HomePageState} State
+ * @type {() => State}
  */
 export const useHomePage = () => {
-
   const {
     isLoading,
     homePageData,
