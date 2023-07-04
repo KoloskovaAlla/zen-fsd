88112742cb575022fadce8ef8116c5d9a5f903ce
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-07-04 19:40:38 +0300

    Refactor Select

--- src/shared/ui/Select/Select.js
+++ src/shared/ui/Select/Select.js
@@ -1,11 +1,11 @@
 import { IconArrow } from 'shared/icons';
 
 /**
- * @typedef {import ('./types').SelectProps} Props
+ * @typedef {import('./types').SelectProps} Props
  * @typedef {import('react').ReactElement} Element
 */
 
-/** @type{({ options, className, onChange, value }: Props) => Element} */
+/** @type {({ options, className, onChange, value }: Props) => Element} */
 
 export const Select = ({ options, className, onChange, value }) => {
   return (
@@ -16,7 +16,7 @@ export const Select = ({ options, className, onChange, value }) => {
       >
         {Object.values(options).map((option) => (
           <option value={option.value} key={option.value}>
-            {option.text && option.text}
+            {option?.text && option.text}
           </option>
         ))}
       </select>
