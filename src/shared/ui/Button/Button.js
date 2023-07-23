08836d5b1d950fd329523f56bfd9155f10ffc533
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-07-23 13:33:28 +0300

    Refactor Button

--- src/shared/ui/Button/Button.js
+++ src/shared/ui/Button/Button.js
@@ -1,20 +1,24 @@
 import { IconClose, IconMoon, IconSun } from 'shared/icons/';
+
+const icons = {
+  close: <IconClose />,
+  sun: <IconSun />,
+  moon: <IconMoon />
+}; 
+
 /**
  * @typedef {import('./types').ButtonProps} Props
  * @typedef {import('react').ReactElement} Element
  */
 
-/** @type {({ onClickButton, className, iconName }: Props) => Element} */
+/** @type {(props: Props) => Element} */
 export const Button = ({ onClickButton, className, iconName }) => {
   return (
     <button 
       onClick={onClickButton} 
       className={className}   
-    >      
-      кнопка закрытия
-      {iconName === 'close' && <IconClose />}
-      {iconName === 'sun' && <IconSun />}
-      {iconName === 'moon' && <IconMoon />}
+    >          
+      {icons[iconName]}
     </button>
   )
 }; 
