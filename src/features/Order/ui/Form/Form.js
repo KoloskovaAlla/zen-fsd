b072bdf09dea8f93c271f7b1438ff56e59229501
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-08-03 21:18:08 +0300

    Added formProps

--- src/features/Order/ui/Form/Form.js
+++ src/features/Order/ui/Form/Form.js
@@ -1,99 +1,105 @@
-import { TextField, SelectField } from 'entitiess'; 
-import { Checkbox } from 'shared/ui'; 
-import { Button } from 'shared/ui'; 
+import { TextField, SelectField } from 'entity'; 
+import { Checkbox,  Button } from 'shared/ui'; 
 import classes from './Form.module.scss';
-/** @type any */
-export const Form = ({ 
-  handleFormSubmit, 
-  orderData, 
-  name, 
-  isValidName, 
-  onNameChange, 
-  tel, 
-  isValidTel, 
-  onTelChange, 
-  email, 
-  isValidEmail,   
-  onEmailChange, 
-  connection,   
-  isValidConnection, 
-  onConnectionChange, 
-  isChecked, 
-  setIsChecked, 
-  isSubmitDisabled }) => {  
-    if (!orderData) return;
-    return (
-      <form 
-        onSubmit={handleFormSubmit} 
-        className={classes.form} 
-      >  
-        {orderData?.inputName && ( 
-          <TextField 
-            className={classes.name} 
-            type={orderData.inputName.type} 
-            placeholder={orderData.inputName.placeholder} 
-            label='' 
-            value={name} 
-            isValid={isValidName} 
-            invalidMessage={orderData.inputName.invalidMessage} 
-            onFieldChange={onNameChange} 
-          /> 
-        )} 
-  
-        {orderData?.inputTel && (
-          <TextField 
-            className={classes.tel} 
-            type={orderData.inputTel.type} 
-            placeholder={orderData.inputTel.placeholder} 
-            label='' 
-            value={tel} 
-            isValid={isValidTel} 
-            invalidMessage={orderData.inputTel.invalidMessage} 
-            onFieldChange={onTelChange} 
-          /> 
-        )}      
-  
-        {orderData?.inputEmail && ( 
-          <TextField 
-            className={classes.email} 
-            type={orderData.inputEmail.type} 
-            placeholder={orderData.inputEmail.placeholder} 
-            label='' 
-            value={email} 
-            isValid={isValidEmail} 
-            invalidMessage={orderData.inputEmail.invalidMessage} 
-            onFieldChange={onEmailChange} 
-          /> 
-        )} 
-  
-        {orderData?.connection?.options && ( 
-          <SelectField 
-            className={classes.connection} 
-            value={connection} 
-            label={orderData.connection.label} 
-            options={orderData.connection.options} 
-            isValid={isValidConnection} 
-            invalidMessage={orderData.connection.invalidMessage} 
-            onFieldChange={onConnectionChange} 
-          /> 
-        )} 
-  
-        <label className={classes.policy}> 
-          <Checkbox 
-            isChecked={isChecked} 
-            setIsChecked={setIsChecked} 
+
+/**
+ * @typedef {import('./types').FormProps} Props
+ * @typedef {import('react').ReactElement} Element
+ * @type {(formProps: Props) => Element}
+ */
+export const Form = (formProps) => {  
+  const {   
+    handleFormSubmit, 
+    orderData, 
+    name, 
+    isValidName, 
+    onNameChange, 
+    tel, 
+    isValidTel, 
+    onTelChange, 
+    email, 
+    isValidEmail,   
+    onEmailChange, 
+    connection,   
+    isValidConnection, 
+    onConnectionChange, 
+    isChecked, 
+    setIsChecked, 
+    isSubmitDisabled 
+  } = formProps;
+
+  return (
+    <form 
+      onSubmit={handleFormSubmit} 
+      className={classes.form} 
+    >  
+      {orderData?.inputName && ( 
+        <TextField 
+          className={classes.name} 
+          type={orderData.inputName.type} 
+          placeholder={orderData.inputName.placeholder} 
+          label='' 
+          value={name} 
+          isValid={isValidName} 
+          invalidMessage={orderData.inputName.invalidMessage} 
+          onFieldChange={onNameChange} 
         /> 
-          <a href={orderData?.inputPolicy?.url}> 
-            {orderData?.inputPolicy?.content} 
-          </a> 
-        </label> 
+      )} 
   
-        <Button 
-          className={classes.submit} 
-          label='submit' 
-          content={'text'} 
-          disabled={isSubmitDisabled} 
+      {orderData?.inputTel && (
+        <TextField 
+          className={classes.tel} 
+          type={orderData.inputTel.type} 
+          placeholder={orderData.inputTel.placeholder} 
+          label='' 
+          value={tel} 
+          isValid={isValidTel} 
+          invalidMessage={orderData.inputTel.invalidMessage} 
+          onFieldChange={onTelChange} 
         /> 
-      </form> 
-    );
+      )}      
+  
+      {orderData?.inputEmail && ( 
+        <TextField 
+          className={classes.email} 
+          type={orderData.inputEmail.type} 
+          placeholder={orderData.inputEmail.placeholder} 
+          label='' 
+          value={email} 
+          isValid={isValidEmail} 
+          invalidMessage={orderData.inputEmail.invalidMessage} 
+          onFieldChange={onEmailChange} 
+        /> 
+      )} 
+  
+      {orderData?.connection?.options && ( 
+        <SelectField 
+          className={classes.connection} 
+          value={connection} 
+          label={orderData.connection.label} 
+          options={orderData.connection.options} 
+          isValid={isValidConnection} 
+          invalidMessage={orderData.connection.invalidMessage} 
+          onFieldChange={onConnectionChange} 
+        /> 
+      )} 
+  
+      <label className={classes.policy}> 
+        <Checkbox 
+          isChecked={isChecked} 
+          setIsChecked={setIsChecked} 
+        /> 
+        <a href={orderData?.inputPolicy?.url}> 
+          {orderData?.inputPolicy?.content} 
+        </a> 
+      </label> 
+  
+      <Button 
+        className={classes.submit} 
+        label='submit' 
+        content={'text'} 
+        disabled={isSubmitDisabled} 
+      /> 
+    </form> 
+  );
 };
---
+++ src/features/Order/ui/Form/types.ts
@@ -0,0 +1,19 @@
+export interface FormProps {
+  handleFormSubmit: any;
+  orderData: any;
+  name: any;
+  isValidName: any; 
+  onNameChange: any;
+  tel: any;
+  isValidTel: any; 
+  onTelChange: any;
+  email: any; 
+  isValidEmail: any;   
+  onEmailChange: any; 
+  connection: any;   
+  isValidConnection: any; 
+  onConnectionChange: any; 
+  isChecked: any; 
+  setIsChecked: any;
+  isSubmitDisabled: any;
+};
