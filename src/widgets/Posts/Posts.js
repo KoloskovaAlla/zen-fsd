224daccfb696e74626c4eff36113a237b251f35b
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2023-09-05 17:51:10 +0300

    Deleted extra rows

--- src/widgets/Posts/Posts.js
+++ src/widgets/Posts/Posts.js
@@ -27,82 +27,25 @@ export const Posts = () => {
             {postsData.title && <h2 className={classes.title}>{postsData?.title.content}</h2>}
 
             <ul className={classes.list}>
-              <li>
-                <Link className={classes.post} to='/posts/warranty'>
-                  <button className={classes.image}>
-                    <img
-                      src={postsData?.warranty?.image.source}
-                      alt='alternate img'
-                    ></img>
-                  </button>
-                  <div className={classes.body}>
-                    <button className={classes.titlePost}>
-                      {postsData?.warranty?.title}
-                    </button>
-                    <div className={classes.article}>
-                      {postsData?.warranty?.article.slice(0, 50)}...
-                    </div>
-                    <button className={classes.link}>Read more...</button>
-                  </div>
-                </Link>
-              </li>
-              <li>
-                <Link className={classes.post} to='/posts/care'>
-                  <button className={classes.image}>
-                    <img src={postsData?.care?.image.source} alt='alternate img'></img>
-                  </button>
-                  <div className={classes.body}>
-                    <button className={classes.titlePost}>
-                      {postsData?.care?.title}
-                    </button>
-                    <div className={classes.article}>
-                      {postsData?.care?.article.slice(0, 49)}...
-                    </div>
-                    <button className={classes.link}>Read more...</button>
-                  </div>
-                </Link>
-              </li>
-              <li>
-                <Link className={classes.post} to='/posts/cashback'>
-                  <button className={classes.image}>
-                    <img
-                      src={postsData?.cashback?.image.source}
-                      alt='alternate img'
-                    ></img>
-                  </button>
-                  <div className={classes.body}>
-                    <button className={classes.titlePost}>
-                      {postsData?.cashback?.title}
-                    </button>
-                    <div className={classes.article}>
-                      {postsData?.cashback.article.slice(0, 50)}...
-                    </div>
-                    <button className={classes.link}>Read more...</button>
-                  </div>
-                </Link>
-              </li>
+              {Object.keys(postsData).map((postKey) => {
+                const post = postsData[postKey];
+                console.log(post)
+                return (
+                  <li key={postKey}>
+                    <Link className={classes.post} to={`/posts/${postKey}`}>
+                      <button className={classes.image}>
+                        <img src={post?.image?.source} alt='alternate img' />
+                      </button>
+                      <div className={classes.body}>
+                        <button className={classes.titlePost}>{post.title}</button>
+                        <div className={classes?.article}>{post?.article?.slice(0, 50)}...</div>
+                        <button className={classes.link}>Read more...</button>
+                      </div>
+                    </Link>
+                  </li>
+                );
+              })}
             </ul>
-
-              <ul className={classes.list}>
-                {Object.keys(postsData).map((postKey) => {
-                  const post = postsData[postKey];
-                  console.log(post)
-                  return (
-                    <li key={postKey}>
-                      <Link className={classes.post} to={`/posts/${postKey}`}>
-                        <button className={classes.image}>
-                          <img src={post?.image?.source} alt='alternate img' />
-                        </button>
-                        <div className={classes.body}>
-                          <button className={classes.titlePost}>{post.title}</button>
-                          <div className={classes?.article}>{post?.article?.slice(0, 50)}...</div>
-                          <button className={classes.link}>Read more...</button>
-                        </div>
-                      </Link>
-                    </li>
-                  );
-                })}
-              </ul>
 
             <Link className={classes.post} to='/posts'>
               <button
