083a9922b0f024e16e0d15f86fa75d053b3b6fbb
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2023-09-27 15:11:48 +0300

    Test for validateEmail

--- src/shared/lib/validateEmail/validateEmail.js
+++ src/shared/lib/validateEmail/validateEmail.js
@@ -1,4 +1,4 @@
 export const validateEmail = (email) => {
-  const regex = /[a-z0-9]+@[a-z]+\.(ru|com)/gi;
+  const regex = /^[a-zA-Z0-9]+@[a-z]+\.(ru|com)$/i;
   return regex.test(String(email));
 };
--- src/shared/lib/validateEmail/validateEmail.test.js
+++ src/shared/lib/validateEmail/validateEmail.test.js
@@ -2,14 +2,59 @@ import { validateEmail } from './validateEmail';
 
 const dataForTests = {
   test1: {
-    email: 'fsdrty@mail.ru',
+    email: 'email123@mail.ru',
     expectedValidity: true
-  }
+  },
+  test2: {
+    email: 'email@gmail.com',
+    expectedValidity: true
+  },
+  test3: {
+    email: 'e,mail@mail.ru',
+    expectedValidity: false
+  },
+  test4: {
+    email: 'email@m/ail.ru',
+    expectedValidity: false
+  },
+  test5: {
+    email: 'email@yandex.cn',
+    expectedValidity: false
+  },
+  test6: {
+    email: 'EMAIL@yandex.ru',
+    expectedValidity: true
+  },
 };
 
 describe('validateEmail:', () => {
   it('Unit test 1', () => {
-    const { email, expectedValidity } = dataForTests.test1;    
+    const { email, expectedValidity } = dataForTests.test1;
+    const validity = validateEmail(email);
+    expect(validity).toEqual(expectedValidity);
+  });
+  it('Unit test 2', () => {
+    const { email, expectedValidity } = dataForTests.test2;
+    const validity = validateEmail(email);
+    expect(validity).toEqual(expectedValidity);
+  });
+  it('Unit test 3', () => {
+    const { email, expectedValidity } = dataForTests.test3;
+    const validity = validateEmail(email);
+    expect(validity).toEqual(expectedValidity);
+  });
+  it('Unit test 4', () => {
+    const { email, expectedValidity } = dataForTests.test4;
+    const validity = validateEmail(email);
+    expect(validity).toEqual(expectedValidity);
+  });
+  it('Unit test 5', () => {
+    const { email, expectedValidity } = dataForTests.test5;
+    const validity = validateEmail(email);
+    expect(validity).toEqual(expectedValidity);
+  });
+  it('Unit test 6', () => {
+    const { email, expectedValidity } = dataForTests.test6;
     const validity = validateEmail(email);
     expect(validity).toEqual(expectedValidity);
   });
