e3e44f14d2f10f1a85a45b0d6a93607268278ca3
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-09-27 21:15:12 +0300

    Tests for validateTel

---
+++ src/shared/lib/validateTel/validateTel.test.js
@@ -0,0 +1,52 @@
+import { validateTel } from './validateTel';
+
+const dataForTests = {
+  test1: {
+    tel: '0123456789',
+    expectedValidity: true
+  },
+  test2: {
+    tel: '01234567890',
+    expectedValidity: false
+  },
+  test3: {
+    tel: '012345678',
+    expectedValidity: false
+  },
+  test4: {
+    tel: '012a456789',
+    expectedValidity: false
+  },
+  test5: {
+    tel: '012a456.89',
+    expectedValidity: false
+  },
+};
+
+describe('validateTel:', () => {
+  it('Unit test 1', () => {
+    const { tel, expectedValidity } = dataForTests.test1;
+    const validity = validateTel(tel);
+    expect(validity).toEqual(expectedValidity);
+  });
+  it('Unit test 2', () => {
+    const { tel, expectedValidity } = dataForTests.test2;
+    const validity = validateTel(tel);
+    expect(validity).toEqual(expectedValidity);
+  });
+  it('Unit test 3', () => {
+    const { tel, expectedValidity } = dataForTests.test3;
+    const validity = validateTel(tel);
+    expect(validity).toEqual(expectedValidity);
+  });
+  it('Unit test 4', () => {
+    const { tel, expectedValidity } = dataForTests.test4;
+    const validity = validateTel(tel);
+    expect(validity).toEqual(expectedValidity);
+  });
+  it('Unit test 5', () => {
+    const { tel, expectedValidity } = dataForTests.test5;
+    const validity = validateTel(tel);
+    expect(validity).toEqual(expectedValidity);
+  });
+});
