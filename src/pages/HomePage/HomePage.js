20ae25791c3e5cb9775af936a0958629fa250d14
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-09-30 14:42:34 +0300

    Fixed bugs in the Homepage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,18 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { SectionBase, Cashback } from 'widgets';
-// toDo import Modal from '';
+
 import { useLang, useTheme, useHomePage } from 'shared/model/hooks';
 import { useDispatch } from 'react-redux';
-// toDo import { setCurrentPage } from 'reducers/currentPageSlice';
+
 
 const HomePage = () => {
   const { lang } = useLang();
   const { theme } = useTheme();
+
   const {
     fetchHomePageData,
-    isLoading,
     homePageData,
-    errorMessage,
   } = useHomePage();
   const dispatch = useDispatch();
 
@@ -20,13 +19,16 @@ const HomePage = () => {
     dispatch(fetchHomePageData(lang));
   }, [lang]);
 
+  useEffect(() => {
+    if (homePageData) console.log(homePageData.cashback);
+  }, [homePageData]);
+
   return (
     <div>
       {homePageData?.download && <SectionBase data={homePageData.download} type='primary' />}
       {homePageData?.warranty && <SectionBase data={homePageData.warranty} type='secondary' reverse />}
       {homePageData?.care && <SectionBase data={homePageData.care} type='secondary' />}
       {homePageData?.cashback && <Cashback data={homePageData.cashback} />}
-      {/* {homePageData?.modal && <Modal data={homePageData.modal} />} */}
     </div>
   );
 };
