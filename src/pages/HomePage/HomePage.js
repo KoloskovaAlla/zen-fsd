0efafcf342971577f89cb9933f03b8e164926227
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2023-10-02 12:22:49 +0300

    Refactored HomePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,19 +1,18 @@
 import { useEffect } from 'react';
-import { SectionBase, Cashback } from 'widgets';
-
-import { useLang, useHomePage } from 'shared/model/hooks';
 import { useDispatch } from 'react-redux';
+import { useLang, useHomePage } from 'shared/model/hooks';
+import { SectionBase, Cashback } from 'widgets';
 
 const HomePage = () => {
   const dispatch = useDispatch();
-  
+
   const {
     fetchHomePageData,
     homePageData,
   } = useHomePage();
-  
+
   const { lang } = useLang();
-  
+
   useEffect(() => {
     dispatch(fetchHomePageData(lang));
   }, [lang, fetchHomePageData, dispatch]);
@@ -22,7 +21,7 @@ const HomePage = () => {
     <div>
       {homePageData?.download && <SectionBase data={homePageData.download} type='primary' />}
       {homePageData?.warranty && <SectionBase data={homePageData.warranty} type='secondary' reverse />}
-      {homePageData?.care && <SectionBase data={homePageData.care} type='secondary'/>}
+      {homePageData?.care && <SectionBase data={homePageData.care} type='secondary' />}
       {homePageData?.cashback && <Cashback />}
     </div>
   );
