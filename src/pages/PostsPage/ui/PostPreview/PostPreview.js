dfc8cc78e7fa14e864000bb23f7eb2602dc9994c
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2023-10-04 11:59:25 +0300

    Refactored styles for Posts

--- src/pages/PostsPage/ui/PostPreview/PostPreview.js
+++ src/pages/PostsPage/ui/PostPreview/PostPreview.js
@@ -1,21 +1,21 @@
 import classes from './PostPreview.module.scss';
 import { Link } from 'react-router-dom';
-import { Preview } from 'features';
+import { useEffect } from 'react';
 
 export const PostPreview = ({ details }) => {
-  const { media, title, key } = details;
+  const { image, title, key } = details;
 
   return (
     <Link to={`/posts/${key}`}>
       <div className={classes.postPreview}>
-        <div className={classes.header}>        
-          
-             <div className={classes.image}>
-              <img
-                src={details?.src}
-                alt={details?.alternate}
-              />
-            </div >
+        <div className={classes.header}>
+
+          <div className={classes.image}>
+            <img
+              src={image?.source}
+              alt={image?.alternate}
+            />
+          </div >
         </div>
         <h2 className={classes.title}>{title}</h2>
         <div className={classes.read}>
