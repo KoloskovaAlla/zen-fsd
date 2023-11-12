e5bb57ef230fe84e92c9adf2487fde27e6fef8ff
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-11-12 22:12:27 +0300

    Refactored navSlice

--- src/shared/reducers/navSlice.js
+++ src/shared/reducers/navSlice.js
@@ -1,42 +1,55 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { API_BASE_URL } from 'shared/constants/api';
 
-const getNav = createAsyncThunk(
-  'navigation/getData',
-  async (_, thunkApi) => {
-    /**  @type {*} */
-    const state = thunkApi.getState();
-    const { lang } = state.langsReducer;
-    const url = `${API_BASE_URL}/${lang}/header/menuItems/.json`;
+/**
+ * @typedef {import('./types').ThunkAPI} ThunkAPI
+ * @typedef {import('./types').NavFromAPI} NavFromAPI
+ */
 
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      if (!Object.values(data).length) throw new Error('Data is empty');
-      return thunkApi.fulfillWithValue(data);
-    } catch (error) {
-      console.error(error);
-      /** @type {*} */
-      const { message } = error;
-      return thunkApi.rejectWithValue(message);
-    }
+/**
+ * @function onGetNav
+ * @param {null} _
+ * @param {ThunkAPI} thunkAPI
+ * @returns {Promise<NavFromAPI | string>}
+ */
+
+const onGetNav = async (_, thunkAPI) => {
+  try {
+    const /** @type {*} */ state = thunkAPI.getState();
+    const { lang } = state.langsReducer;
+    const endpoint = `${lang}`;
+    const url = `${API_BASE_URL}/${endpoint}/.json`;
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data.message) throw new Error(data.message);
+    return thunkAPI.fulfillWithValue(data.navItems);
+  } catch (error) {
+    const /** @type {*} */ { message } = error;
+    console.error(message);
+    return thunkAPI.rejectWithValue(message);
   }
+};
+
+/** @type {*} */
+const getNav = createAsyncThunk(
+  'nav/getNav',
+  onGetNav,
 );
 
 /**
-  * @typedef {import('./types').NavState} State
-  * @type {State}
-*/
+  * @typedef {import('./types').NavItem} NavItem
+  */
 
 const initialState = {
   isLoading: false,
-  navItems: null,
+  /** @type {[] | NavItem[]} */
+  navItems: [],
   errorMessage: '',
   isNavActive: false,
 };
 
 const navSlice = createSlice({
-  name: 'navigation',
+  name: 'nav',
   initialState,
   reducers: {
     setIsNavActive: (state, { payload }) => {
@@ -44,19 +57,19 @@ const navSlice = createSlice({
     },
   },
   extraReducers: {
-    [`${getNav.pending}`]: (state) => {
+    [getNav.pending]: (state) => {
       state.isLoading = true;
-      state.navItems = null;
+      state.navItems = [];
       state.errorMessage = '';
     },
-    [`${getNav.fulfilled}`]: (state, { payload }) => {
+    [getNav.fulfilled]: (state, { payload }) => {
       state.isLoading = false;
       state.navItems = payload;
       state.errorMessage = '';
     },
-    [`${getNav.rejected}`]: (state, { payload }) => {
+    [getNav.rejected]: (state, { payload }) => {
       state.isLoading = false;
-      state.navItems = null;
+      state.navItems = [];
       state.errorMessage = payload;
     },
   }
