a1fbb03df82cd59afca6cef66fa8cdd61060d5df
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2023-11-30 17:57:20 +0300

    Started to developed Modal

--- src/app/App.js
+++ src/app/App.js
@@ -19,7 +19,7 @@ export const App = () => {
   const { postErrorMessage } = usePost();
 
   useEffect(() => {
-    if (postErrorMessage !== '') {
+    if (postErrorMessage !== '' && postErrorMessage !== null) {
       setErrorMessage(postErrorMessage);
       setIsErrorMessage(true);
     }
