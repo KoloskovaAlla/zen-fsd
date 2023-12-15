904839c9a5cf73ed3a829d4cbd470805a4e7c9a0
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2023-12-15 17:54:50 +0300

    Refactor error modal in the App

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,7 @@
 import './styles/index.scss';
 import { useState, useEffect } from 'react';
-import { useTheme, usePost } from 'shared/hooks';
+import { useTheme, usePost, useLang } from 'shared/hooks';
+import { POST_ERRORS } from 'shared/constants/api';
 import { Order, Modal } from 'features';
 import { Header, Footer, Clients, Posts } from 'widgets';
 import { Router } from 'pages';
@@ -11,18 +12,23 @@ import { Router } from 'pages';
  */
 
 export const App = () => {
-  const [errorMessage, setErrorMessage] = useState('');
-  const [isModalActive, setIsModalActive] = useState(false);
-
   const { theme } = useTheme();
+  const { lang } = useLang();
   const { postErrorMessage } = usePost();
 
+  const [displayedErrorMessage, setDisplayedErrorMessage] = useState('');
+  const [isModalActive, setIsModalActive] = useState(false);
+
+  const { modalTitle, postDisplayedErrorMessage } = POST_ERRORS[lang];
+
   useEffect(() => {
-    if (postErrorMessage !== '' && postErrorMessage !== null) {
-      setErrorMessage('Поста пока нет, но он скоро здесь появится');
+    const isPostError = postErrorMessage !== '' && postErrorMessage !== null;
+
+    if (isPostError) {
       setIsModalActive(true);
+      setDisplayedErrorMessage(postDisplayedErrorMessage);
     }
-  }, [postErrorMessage]);
+  }, [postErrorMessage, lang]);
 
   return (
     <div className={`app ${theme}`}>
@@ -34,10 +40,10 @@ export const App = () => {
       <Order />
       {isModalActive && (
         <Modal
-          isModalActive={isModalActive}
+          isModalActive
           setIsModalActive={setIsModalActive}
-          title={`Сообщение об ошибке`}
-          content={errorMessage}
+          title={modalTitle}
+          content={displayedErrorMessage}
         />
       )}
     </div >
