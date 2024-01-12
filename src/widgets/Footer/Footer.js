58db624f8a37a5483729cc5b03895aefb4d3dce1
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2024-01-12 12:23:51 +0300

    Added changing langs in the Footer

--- src/widgets/Footer/Footer.js
+++ src/widgets/Footer/Footer.js
@@ -1,7 +1,7 @@
 import classes from './Footer.module.scss';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { useColumns, useInfo } from 'shared/hooks';
+import { useColumns, useInfo, useLang } from 'shared/hooks';
 import { Column, Developer } from './ui';
 import { IconLogoFooter } from 'shared/icons';
 
@@ -14,11 +14,12 @@ export const Footer = () => {
   const dispatch = useDispatch();
   const { getColumns, columns } = useColumns();
   const { getInfo, info } = useInfo();
+  const { lang } = useLang();
 
   useEffect(() => {
     dispatch(getColumns());
     dispatch(getInfo());
-  }, [dispatch, getColumns, getInfo]);
+  }, [dispatch, getColumns, getInfo, lang]);
 
   return (
     <footer className={classes.footer}>
